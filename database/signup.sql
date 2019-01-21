c5d3987c7141260576b2e63a09d28df058460b6c
Saikat Singha Mahapatra <saikat.singhamahapatra@cognizant.com>
2019-01-21 23:02:33 +0530

    fixed sql script

--- database/signup.sql
+++ database/signup.sql
@@ -2,19 +2,19 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-DROP SCHEMA IF EXISTS `signupv1` ;
-CREATE SCHEMA IF NOT EXISTS `signupv1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `signupv1` ;
+DROP SCHEMA IF EXISTS `signup` ;
+CREATE SCHEMA IF NOT EXISTS `signup` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `signup` ;
 
 -- -----------------------------------------------------
--- Table `signupv1`.`user`
+-- Table `signup`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `signupv1`.`user` ;
+DROP TABLE IF EXISTS `signup`.`user` ;
 
-CREATE  TABLE IF NOT EXISTS `signupv1`.`user` (
+CREATE  TABLE IF NOT EXISTS `signup`.`user` (
   `us_id` INT NOT NULL AUTO_INCREMENT ,
   `us_name` VARCHAR(45) NULL ,
-  `us_email` VARCHAR(45) NULL ,
+  `us_email` VARCHAR(45) NULL UNIQUE,
   `us_password` VARCHAR(45) NULL ,
   PRIMARY KEY (`us_id`) ,
   UNIQUE INDEX `us_email_UNIQUE` (`us_email` ASC) )
